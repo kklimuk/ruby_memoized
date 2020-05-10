b602f1e9a0d2ab68ab877e6bbc8f1adb7daf1776
Kirill Klimuk <kklimuk@gmail.com>
2020-05-09 17:38:19 -0700

    Bump for ruby 2.7 keyword args

--- spec/ruby_memoized/memoizer_spec.rb
+++ spec/ruby_memoized/memoizer_spec.rb
@@ -15,41 +15,46 @@ describe RubyMemoized::Memoizer do
 
   let(:method) { :american_exceptionalism }
 
-  subject { memoizer.call(*args, &block) }
+  subject { memoizer.call(*args, **kwargs, &block) }
 
-  shared_examples 'a memoized method' do |args, block, value|
+  shared_examples 'a memoized method' do |args, kwargs, block, value|
     let(:args) { args }
+    let(:kwargs) { kwargs }
     let(:block) { block }
     let(:value) { value }
 
     it 'caches the value' do
       is_expected.to eq value
-      expect(memoizer.cache).to include([args, block] => value)
+      expect(memoizer.cache).to include([args, kwargs, block] => value)
     end
 
     context 'and the subject has already run once' do
-      before { memoizer.call(*args, &block) }
+      before { memoizer.call(*args, **kwargs, &block) }
 
       it 'uses the cache if the value has been computed' do
-        expect(memoizer.cache).to receive(:[]).with([args, block]).and_call_original
+        expect(memoizer.cache).to receive(:[]).with([args, kwargs, block]).and_call_original
         is_expected.to eq value
       end
     end
   end
 
   context 'when there are no arguments or blocks' do
-    include_examples 'a memoized method', [], nil, 26
+    include_examples 'a memoized method', [], {}, nil, 26
   end
 
   context 'when there are arguments' do
-    include_examples 'a memoized method', [1, 2, 3], nil, 34
+    include_examples 'a memoized method', [1, 2, 3], {}, nil, 34
+  end
+
+  context 'when there are only keyword arguments' do
+    include_examples 'a memoized method', [], { foo: :bar }, nil, 26
   end
 
   context 'when there is a block' do
-    include_examples 'a memoized method', [], proc {}, 36
+    include_examples 'a memoized method', [], {}, proc {}, 36
   end
 
   context 'when there are arguments and a block' do
-    include_examples 'a memoized method', [1, 2, 3], proc {}, 53
+    include_examples 'a memoized method', [1, 2, 3], {}, proc {}, 53
   end
 end
