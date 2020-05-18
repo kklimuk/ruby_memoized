29e59e10a9c6f28f6a5af210c4038fa116c6447e
tvl2386 <drabber2000@gmail.com>
2020-05-18 20:27:42 +0200

    added support for methods without arguments

--- lib/ruby_memoized/memoizer.rb
+++ lib/ruby_memoized/memoizer.rb
@@ -1,15 +1,21 @@
 module RubyMemoized
   class Memoizer
-    attr_reader :context, :method
+    attr_reader :context, :method, :accepts_arguments
 
-    def initialize(context, method)
+    def initialize(context, method, accepts_arguments)
       @context = context
       @method = method
+      @accepts_arguments = accepts_arguments
     end
 
     def call(*args, **kwargs, &block)
       return cache[[args, kwargs, block]] if cache.has_key?([args, kwargs, block])
-      cache[[args, kwargs, block]] = context.send(method, *args, **kwargs, &block)
+
+      if accepts_arguments
+        cache[[args, kwargs, block]] = context.send(method, *args, **kwargs, &block)
+      else
+        cache[[args, kwargs, block]] = context.send(method)
+      end
     end
 
     def cache
